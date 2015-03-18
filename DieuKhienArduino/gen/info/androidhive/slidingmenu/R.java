acf0392f338e3857e1e519f469e7681d5bfde76b
quytutlu <quytutlu@gmail.com>
2015-03-18 23:44:57 +0700

    Thêm backgroud

--- DieuKhienArduino/gen/info/androidhive/slidingmenu/R.java
+++ DieuKhienArduino/gen/info/androidhive/slidingmenu/R.java
@@ -38,18 +38,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int counter_bg=0x7f020000;
-        public static final int ic_communities=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_home=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_pages=0x7f020005;
-        public static final int ic_people=0x7f020006;
-        public static final int ic_photos=0x7f020007;
-        public static final int ic_whats_hot=0x7f020008;
-        public static final int list_item_bg_normal=0x7f020009;
-        public static final int list_item_bg_pressed=0x7f02000a;
-        public static final int list_selector=0x7f02000b;
+        public static final int backgroud=0x7f020000;
+        public static final int counter_bg=0x7f020001;
+        public static final int ic_communities=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_home=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_pages=0x7f020006;
+        public static final int ic_people=0x7f020007;
+        public static final int ic_photos=0x7f020008;
+        public static final int ic_whats_hot=0x7f020009;
+        public static final int list_item_bg_normal=0x7f02000a;
+        public static final int list_item_bg_pressed=0x7f02000b;
+        public static final int list_selector=0x7f02000c;
     }
     public static final class id {
         public static final int Button02=0x7f0a0034;
